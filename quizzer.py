2bb5c5bdc65a34a8d922451b1bf6477b00c51dc6
yhjeon <yuno96@gmail.com>
2019-01-03 08:17:01 +0900

    GUI done

--- quizzer.py
+++ quizzer.py
@@ -10,6 +10,7 @@ from tkinter import *
 #from tempfile import gettempdir
 import openpyxl
 from tkinter.messagebox import showinfo
+from tkinter import filedialog
 
 class Quizzer:
 	def __init__(self, root=None):
@@ -23,6 +24,7 @@ class Quizzer:
 		self.root.title('quizzer')
 		self.qna_pool = {}
 		self.q_str = StringVar()
+		self.fd_excel = None
 
 		self.logging.basicConfig(level=logging.DEBUG,
 			format='%(levelname)s:%(filename)s:%(funcName)s:%(lineno)d:%(message)s')
@@ -32,7 +34,7 @@ class Quizzer:
 		self.menubar = Menu(root)
 		menu_file = Menu(self.menubar)
 		menu_file.add_command(label='Open DB', command=self.open_db)
-		menu_file.add_command(label='Open DB', command=self.root_quit)
+		menu_file.add_command(label='Exit', command=self.root.quit)
 		self.menubar.add_cascade(label='File', menu=menu_file)
 		## Help
 		about_menu = Menu(self.menubar)
@@ -43,23 +45,23 @@ class Quizzer:
 		# BODY
 		self.q_str.set('Q0:Q0')
 		tmp = Label(root, textvariable=self.q_str)
-		tmp.pack(side=LEFT)
+		tmp.grid(row=0, column=0)
 		sbar = Scrollbar(root)
-		sbar.pack(side=Right, fill=Y)
-		self.text = Text(root, yscrollcommand=scrollbar.set)
-		self.text.pack(side=LEFT, fill=BOTH)
+		sbar.grid(row=0,column=2, sticky=N+S)
+		self.text = Text(root, height=3, bg='lightgray', yscrollcommand=sbar.set)
+		self.text.grid(row=0, column=1)
 		sbar.config(command=self.text.yview)
 
-		tmp = Label(root, Text='Answer:')
-		tmp.pack(side=LEFT)
+		tmp = Label(root, text='Answer:')
+		tmp.grid(row=1, column=0)
 		self.myans = Entry(root)
-		self.myans.pack(side=LEFT, fill=BOTH)
+		self.myans.grid(row=1, column=1, sticky=W+E)
 		self.myans.bind('<Enter>', self.eval_myans)
 
-		self.ans = Entry(root)
-		self.ans.pack(side=LEFT, fill=BOTH)
+		self.ans = Entry(root, bg='lightgray')
+		self.ans.grid(row=2, column=1, sticky=W+E)
 
-        def show_about(self):
+	def show_about(self):
 		about_msg = '''
 		\rYou can get the latest version here:
 		\rhttps://github.com/yuno96/quizzer.git
@@ -68,26 +70,31 @@ class Quizzer:
 
 	def open_db(self):
 		self.logging.debug('yep')
-		fname = os.path.join(self.DBPATH, 'sampledb.xlsx')
+		fname = filedialog.askopenfilename(initialdir=self.DBPATH, title='Select db file')
+		self.logging.debug(fname)
+		#fname = os.path.join(self.DBPATH, 'sampledb.xlsx')
 		if not os.path.exists(fname):
-			self.logging.warning('There is not file')
+			self.logging.warning('There is nofile')
+			return
 
-		if not self.fd_excel:
+		if self.fd_excel:
 			self.fd_excel.close()
 
-		wb = openpyxl.load_workbook(fname)
+		self.fd_excel = openpyxl.load_workbook(fname)
+		self.logging.debug(type(self.fd_excel))
 		# Get active sheet
-		ws = wb.active
-		#ws = wb.get_sheet_by_name('Sheeet')
+		ws = self.fd_excel.active
+		#ws = self.fd_excel.get_sheet_by_name('Sheeet')
 		cnt = 0
 		for r in ws.rows:
 			if not r[0].value:
 				continue
-			self.qna_pool.append({cnt:[r[0].value, r[1].value]})
+			self.qna_pool[cnt] = [r[0].value, r[1].value]
 			cnt += 1
 			print ('%s %s' % (r[0].value, r[1].value))
-		wb.close()
+		self.fd_excel.close()
 
+		print (self.qna_pool)
 		self.q_str.set('Question 0/%d:'%cnt)
 
 	def eval_myans(self):
@@ -104,8 +111,8 @@ if __name__ == '__main__':
 	root.tk.call('encoding', 'system', 'utf-8')
 	#root.option_add( "*font", "lucida 9" )
 	
-	#quizzer = Quizzer(root)
-	quizzer = Quizzer(None)
+	quizzer = Quizzer(root)
+	#quizzer = Quizzer(None)
 	#quizzer.run()
 
 	root.mainloop()
